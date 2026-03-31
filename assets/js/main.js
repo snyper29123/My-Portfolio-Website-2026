7733938862d51a4f7b5fd4a7e5c6a587ac189384
Reynald Albaran <72857015+snyper29123@users.noreply.github.com>
2026-03-31 15:09:52 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,30 @@
 /*=============== HOME SPLIT TEXT ===============*/
+const { animate, splitText, stagger } = anime
 
+const { chars: chars1 } = splitText('.home__professional-1', { chars: true })
+const { chars: chars2 } = splitText('.home__professional-2', { chars: true })
+
+animate(chars1, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 4000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
+
+animate(chars2, {
+  y: [
+    { to: ['100%', '0%'] },
+    { to: '-100%', delay: 5000, ease: 'in(3)' }
+  ],
+  duration: 900,
+  ease: 'out(3)',
+  delay: stagger(80),
+  loop: true,
+});
 
 /*=============== SWIPER PROJECTS ===============*/
 
