363a429de2de29f0f3b41f4567e8f766b71b55b9
Reynald Albaran <72857015+snyper29123@users.noreply.github.com>
2026-04-04 12:41:51 +0800

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -139,12 +139,75 @@ const textYear = document.getElementById('footer-year'),
 textYear.textContent = currentYear
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
 
+const scrollActive = () => {
+  //We get the position by scrolling down
+  const scrollY = window.scrollY
+
+  sections.forEach(section => {
+    const id = section.id, // id of each section
+          top = section.offsetTop - 50, //Distance from the top edge
+          height = section.offsetHeight, // Element height
+          link = document.querySelector('.nav__menu a[href*=' + id + ']') // id nav link
+    
+    if(!link) return
+
+    link.classList.toggle('active-link', scrollY > top && scrollY <= top + height)
+  })
+}
+window.addEventListener('scroll', scrollActive)
 
 /*=============== CUSTOM CURSOR ===============*/
+const cursor = document.querySelector('.cursor')
+let mouseX =0, mouseY = 0 // store mouse position
 
+const cursorMove = () => {
+  //Position the cursor
+  cursor.style.left = `${mouseX}px`
+  cursor.style.top = `${mouseY}px`
+  cursor.style.transform = 'translate(-50%, -50%'
+
+  requestAnimationFrame(cursorMove)
+}
+
+document.addEventListener('mousemove', (e) => {
+  mouseX = e.clientX
+  mouseY = e.clientY
+})
+
+cursorMove()
 
 /* Hide custom cursor on links */
+const a = document.querySelectorAll('a')
 
+a.forEach(item => {
+  item.addEventListener('mouseover', () => {
+    cursor.classList.add('hide-cursor')
+  })
+
+  item.addEventListener('mouseleave', () => {
+    cursor.classList.remove('hide-cursor')
+  })
+})
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2000,
+  delay: 300,
+
+  // reset: true, // Animations repeat
+
+})
+
+sr.reveal(`.home__image, .projects__container, .work__container,
+          .testimonials__container, .contact__container`)
+sr.reveal(`.home__data`, {delay: 900, origin: 'bottom'})
+sr.reveal(`.home__info`, {delay: 1200, origin: 'bottom'})
+sr.reveal(`.home__social, .home__cv`, {delay: 1500})
+sr.reveal(`.about__data`, {origin: 'left'})
+sr.reveal(`.about__image`, {origin: 'right'})
+sr.reveal(`.services__card`, {interval: 100})
+
